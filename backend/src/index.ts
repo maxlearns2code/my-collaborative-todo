15aff976d6605f94d14d06683b0abdaa61ed94d5
Maxime Dewynter <maxdevvynter@gmail.com>
2025-11-24 09:34:50 +0100

    chore: add startup log for Cloud Run debugging

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,6 +7,8 @@ import usersRoutes from "./routes/users.js";
 
 dotenv.config();
 
+console.log("Starting server, PORT =", process.env.PORT);
+
 const app = express();
 app.use(cors());
 app.use(express.json());
